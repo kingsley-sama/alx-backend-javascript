17c833ecbe7f6c92f8ea7c59049f89acaee4abdd
Kingsley Okpo <kngsley2018@gmail.com>
2024-09-17 10:04:10 +0100

    initial commits

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env Node
+function displayMessage(arg) {
+  console.log(arg);
+}
+module.exports = displayMessage;
---
+++ 0x05-Node_JS_basic/0-main.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env Node
+const displayMessage = require('./0-console');
+
+displayMessage("Hello NodeJS!");
